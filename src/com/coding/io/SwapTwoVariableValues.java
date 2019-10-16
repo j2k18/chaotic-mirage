d8e7e5e09f14929d71d07fecb47aff1be902cac1
jyoti <jyotiprakashrout434@gmail.com>
2019-10-16 18:57:59 +0530

    committing the code for swapping two variable values without third variable

---
+++ src/com/coding/io/SwapTwoVariableValues.java
@@ -0,0 +1,19 @@
+package com.coding.io;
+
+public class SwapTwoVariableValues {
+
+    public static void main(String[] args) {
+
+        int x = 375;
+        int y = 371;
+
+         x = x+y;
+         y = x-y;
+         x = x-y;
+
+
+        System.out.println(x);
+        System.out.println(y);
+
+    }
+}
